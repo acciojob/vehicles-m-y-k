8f8361117051d6a53ef30106c0a2ce7da6673c0f
Yusuf <mohammad.yusufkhan12345@gmail.com>
2023-04-20 18:12:53 +0530

    changes

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -5,6 +5,23 @@ public class Car extends Vehicle {
     private String type;
     private int doors;
     private int gears;
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
     private boolean isManual;
     private int currentGear;
 
